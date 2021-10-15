6f81fc87b622f4ec4f6e673438aa596a89665995
Vikneswaran-123 <75965687+Vikneswaran-123@users.noreply.github.com>
2021-10-15 13:02:23 +0530

    feat: dashboard completed with fetching data

--- components/Dashboard.vue
+++ components/Dashboard.vue
@@ -182,7 +182,7 @@
                       tracking-wider
                     "
                   >
-                    User
+                    UserId
                   </th>
                   <th
                     class="
@@ -197,7 +197,7 @@
                       tracking-wider
                     "
                   >
-                    Rol
+                    ID
                   </th>
                   <th
                     class="
@@ -212,7 +212,7 @@
                       tracking-wider
                     "
                   >
-                    Created at
+                    Title
                   </th>
                   <th
                     class="
@@ -227,229 +227,43 @@
                       tracking-wider
                     "
                   >
-                    Status
+                    Body
                   </th>
                 </tr>
               </thead>
-              <tbody>
+              <tbody v-for="post of posts" :key="post.id">
                 <tr>
                   <td
                     class="px-5 py-5 border-b border-gray-200 bg-white text-sm"
                   >
                     <div class="flex items-center">
                       <div class="flex-shrink-0 w-10 h-10">
-                        <img
-                          class="w-full h-full rounded-full"
-                          src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80"
-                          alt=""
-                        />
-                      </div>
-                      <div class="ml-3">
-                        <p class="text-gray-900 whitespace-no-wrap">
-                          Vera Carpenter
-                        </p>
+                        <p>{{ post.userId }}</p>
                       </div>
                     </div>
                   </td>
                   <td
                     class="px-5 py-5 border-b border-gray-200 bg-white text-sm"
                   >
-                    <p class="text-gray-900 whitespace-no-wrap">Admin</p>
-                  </td>
-                  <td
-                    class="px-5 py-5 border-b border-gray-200 bg-white text-sm"
-                  >
-                    <p class="text-gray-900 whitespace-no-wrap">Jan 21, 2020</p>
-                  </td>
-                  <td
-                    class="px-5 py-5 border-b border-gray-200 bg-white text-sm"
-                  >
-                    <span
-                      class="
-                        relative
-                        inline-block
-                        px-3
-                        py-1
-                        font-semibold
-                        text-green-900
-                        leading-tight
-                      "
-                    >
-                      <span
-                        aria-hidden
-                        class="
-                          absolute
-                          inset-0
-                          bg-green-200
-                          opacity-50
-                          rounded-full
-                        "
-                      ></span>
-                      <span class="relative">Activo</span>
-                    </span>
-                  </td>
-                </tr>
-                <tr>
-                  <td
-                    class="px-5 py-5 border-b border-gray-200 bg-white text-sm"
-                  >
-                    <div class="flex items-center">
-                      <div class="flex-shrink-0 w-10 h-10">
-                        <img
-                          class="w-full h-full rounded-full"
-                          src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80"
-                          alt=""
-                        />
-                      </div>
-                      <div class="ml-3">
-                        <p class="text-gray-900 whitespace-no-wrap">
-                          Blake Bowman
-                        </p>
-                      </div>
+                    <div class="ml-3">
+                      <p class="text-gray-900 whitespace-no-wrap">
+                        {{ post.id }}
+                      </p>
                     </div>
                   </td>
                   <td
                     class="px-5 py-5 border-b border-gray-200 bg-white text-sm"
                   >
-                    <p class="text-gray-900 whitespace-no-wrap">Editor</p>
+                    <p class="text-gray-900 whitespace-no-wrap">
+                      {{ post.title }}
+                    </p>
                   </td>
                   <td
                     class="px-5 py-5 border-b border-gray-200 bg-white text-sm"
                   >
-                    <p class="text-gray-900 whitespace-no-wrap">Jan 01, 2020</p>
-                  </td>
-                  <td
-                    class="px-5 py-5 border-b border-gray-200 bg-white text-sm"
-                  >
-                    <span
-                      class="
-                        relative
-                        inline-block
-                        px-3
-                        py-1
-                        font-semibold
-                        text-green-900
-                        leading-tight
-                      "
-                    >
-                      <span
-                        aria-hidden
-                        class="
-                          absolute
-                          inset-0
-                          bg-green-200
-                          opacity-50
-                          rounded-full
-                        "
-                      ></span>
-                      <span class="relative">Activo</span>
-                    </span>
-                  </td>
-                </tr>
-                <tr>
-                  <td
-                    class="px-5 py-5 border-b border-gray-200 bg-white text-sm"
-                  >
-                    <div class="flex items-center">
-                      <div class="flex-shrink-0 w-10 h-10">
-                        <img
-                          class="w-full h-full rounded-full"
-                          src="https://images.unsplash.com/photo-1540845511934-7721dd7adec3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80"
-                          alt=""
-                        />
-                      </div>
-                      <div class="ml-3">
-                        <p class="text-gray-900 whitespace-no-wrap">
-                          Dana Moore
-                        </p>
-                      </div>
-                    </div>
-                  </td>
-                  <td
-                    class="px-5 py-5 border-b border-gray-200 bg-white text-sm"
-                  >
-                    <p class="text-gray-900 whitespace-no-wrap">Editor</p>
-                  </td>
-                  <td
-                    class="px-5 py-5 border-b border-gray-200 bg-white text-sm"
-                  >
-                    <p class="text-gray-900 whitespace-no-wrap">Jan 10, 2020</p>
-                  </td>
-                  <td
-                    class="px-5 py-5 border-b border-gray-200 bg-white text-sm"
-                  >
-                    <span
-                      class="
-                        relative
-                        inline-block
-                        px-3
-                        py-1
-                        font-semibold
-                        text-orange-900
-                        leading-tight
-                      "
-                    >
-                      <span
-                        aria-hidden
-                        class="
-                          absolute
-                          inset-0
-                          bg-orange-200
-                          opacity-50
-                          rounded-full
-                        "
-                      ></span>
-                      <span class="relative">Suspended</span>
-                    </span>
-                  </td>
-                </tr>
-                <tr>
-                  <td class="px-5 py-5 bg-white text-sm">
-                    <div class="flex items-center">
-                      <div class="flex-shrink-0 w-10 h-10">
-                        <img
-                          class="w-full h-full rounded-full"
-                          src="https://images.unsplash.com/photo-1522609925277-66fea332c575?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&h=160&w=160&q=80"
-                          alt=""
-                        />
-                      </div>
-                      <div class="ml-3">
-                        <p class="text-gray-900 whitespace-no-wrap">
-                          Alonzo Cox
-                        </p>
-                      </div>
-                    </div>
-                  </td>
-                  <td class="px-5 py-5 bg-white text-sm">
-                    <p class="text-gray-900 whitespace-no-wrap">Admin</p>
-                  </td>
-                  <td class="px-5 py-5 bg-white text-sm">
-                    <p class="text-gray-900 whitespace-no-wrap">Jan 18, 2020</p>
-                  </td>
-                  <td class="px-5 py-5 bg-white text-sm">
-                    <span
-                      class="
-                        relative
-                        inline-block
-                        px-3
-                        py-1
-                        font-semibold
-                        text-red-900
-                        leading-tight
-                      "
-                    >
-                      <span
-                        aria-hidden
-                        class="
-                          absolute
-                          inset-0
-                          bg-red-200
-                          opacity-50
-                          rounded-full
-                        "
-                      ></span>
-                      <span class="relative">Inactive</span>
-                    </span>
+                    <p class="text-gray-900 whitespace-no-wrap">
+                      {{ post.body }}
+                    </p>
                   </td>
                 </tr>
               </tbody>
@@ -506,3 +320,25 @@
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      posts: [],
+    }
+  },
+  async created() {
+    try {
+      const res = await this.$axios.get(
+        'https://jsonplaceholder.typicode.com/posts'
+      )
+      this.posts = res.data
+      console.log(res)
+    } catch (er) {
+      console.log(er)
+    }
+  },
+}
+</script>
